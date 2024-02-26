77677463c24a4d46546b512f4bdfc18cc97e4174
Profex993 <68472681+Profex993@users.noreply.github.com>
2024-02-26 20:52:01 +0100

    code cleanup

--- src/Hra.java
+++ src/Hra.java
@@ -21,48 +21,48 @@ public class Hra {
         Scanner scn = new Scanner(System.in);
         Random rnd = new Random();
 
-        do{
-            if(populace <= (popMinRok * 0.5)){
+        do {
+            if (populace <= (popMinRok * 0.5)) {
                 break;
             }
             uroda2 = sazeni * uroda;
             busle += uroda2;
             popMinRok = populace;
-            cenaArku = rnd.nextInt(9)+17;
-            uroda = rnd.nextInt(2)+3;
-            System.out.println("rok vlády: "+ rok + "\npočet obyvatel: " + populace + "\n počet hladovějicích obyvatel za minulý rok: " + hlad + "\n počet imigrantů za minulý rok: " + imigranti + "\n počet akrů země: " + pocetArku + "\n ůroda: " + uroda + "\n počet bušlů na skladě: " + busle + "\n cena za jeden akr: " + cenaArku);
-            System.out.println("zvol kolik akrů chceš prodat:     *cena akru = " + cenaArku+" počet bušlů: "+busle+"*");
+            cenaArku = rnd.nextInt(9) + 17;
+            uroda = rnd.nextInt(2) + 3;
+            System.out.println("rok vlády: " + rok + "\npočet obyvatel: " + populace + "\n počet hladovějicích obyvatel za minulý rok: " + hlad + "\n počet imigrantů za minulý rok: " + imigranti + "\n počet akrů země: " + pocetArku + "\n ůroda: " + uroda + "\n počet bušlů na skladě: " + busle + "\n cena za jeden akr: " + cenaArku);
+            System.out.println("zvol kolik akrů chceš prodat:     *cena akru = " + cenaArku + " počet bušlů: " + busle + "*");
             prodejArku = scn.nextInt();
-            if(prodejArku < 0){
+            if (prodejArku < 0) {
                 prodejArku = 0;
             }
             pocetArku -= prodejArku;
             busle += prodejArku * cenaArku;
-            System.out.println("počet bušlů: "+busle);
+            System.out.println("počet bušlů: " + busle);
             System.out.println("zvol kolik bušlů obilí dáš obyvatelům:");
             proOb = scn.nextInt();
-            if(proOb < 0){
+            if (proOb < 0) {
                 proOb = 0;
             }
             busle -= proOb;
-            if(proOb >= populace * 20){
+            if (proOb >= populace * 20) {
                 imigranti = (proOb / 20) - populace;
                 populace += imigranti;
             }
-            if(proOb < populace * 20){
+            if (proOb < populace * 20) {
                 hlad = (populace * 20 - proOb) / 20;
                 populace -= hlad;
             }
-            System.out.println("zvol kolik bušlů obilí dáš na nákup akrů:    *cena akru = " +  cenaArku+" počet bušlů: "+busle+"*");
+            System.out.println("zvol kolik bušlů obilí dáš na nákup akrů:    *cena akru = " + cenaArku + " počet bušlů: " + busle + "*");
             naNakup = scn.nextInt();
-            if(naNakup < 0){
+            if (naNakup < 0) {
                 naNakup = 0;
             }
             pocetArku += naNakup / cenaArku;
             busle -= naNakup;
-            System.out.println("zvol kolik bušlů obilí dáš na sázení:     *počet bušlů: "+busle+"*");
+            System.out.println("zvol kolik bušlů obilí dáš na sázení:     *počet bušlů: " + busle + "*");
             sazeni = scn.nextInt();
-            if(sazeni < 0){
+            if (sazeni < 0) {
                 sazeni = 0;
             }
             System.out.println("""
@@ -70,26 +70,26 @@ public class Hra {
 
 
                     """);
-            if(pocetArku < sazeni){
+            if (pocetArku < sazeni) {
                 sazeni = pocetArku;
             }
             busle -= sazeni;
             rok++;
-        }while(rok<10);
-        if(rok < 10){
+        } while (rok < 10);
+        if (rok < 10) {
             System.out.println("""
 
                     prohrali jsi
                     """);
             int moznost = rnd.nextInt(2);
-            if(moznost == 0){
+            if (moznost == 0) {
                 System.out.println("Obyvatele se rozhodli pro tvou defenestraci.");
             }
-            if(moznost == 1){
+            if (moznost == 1) {
                 System.out.println("Obyvatele tě svrhli z trůnu.");
             }
         }
-        if(rok == 10){
+        if (rok == 10) {
             System.out.println("""
 
                     vyhral jsi
