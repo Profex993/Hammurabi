e1da59a390566c7e8930343d3e17adb77a422ae0
Profex993 <68472681+Profex993@users.noreply.github.com>
2024-02-26 20:51:34 +0100

    initial

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("vítejte ve hře Hammurabi");
+        Hra hra = new Hra();
+        hra.zacitHru();
+    }
+}
